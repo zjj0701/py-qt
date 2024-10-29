e609da03b850e7ea26918de2be90f5ceb7e27029
zjj <544562474@qq.com>
2024-10-29 17:42:42 +0800

    fixed

--- 加载dialogbutton.py
+++ 加载dialogbutton.py
@@ -4,10 +4,15 @@ from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication, QComboBox
 
 if __name__ == '__main__':
+
     app = QApplication(sys.argv)
+
     ui = uic.loadUi('./ui/combox.ui')
     combox:QComboBox = ui.comboBox
     combox.addItem("sb")
     combox.addItems(["sb1","sb2"])
+    print(combox.currentText(),combox.currentIndex())
     ui.show()
     app.exec()
+    print("end...")
+    combox.clear()
