f070947bd6e2ac64a0800231c147dfc07a86285e
zjj <544562474@qq.com>
2024-10-29 17:25:34 +0800

    多选qcheckbox

---
+++ qcheckbox.py
@@ -0,0 +1,11 @@
+import sys
+
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ui = uic.loadUi('./ui/checkbox.ui')
+
+    ui.show()
+    app.exec()
