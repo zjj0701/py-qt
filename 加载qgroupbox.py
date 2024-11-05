a5ccf3c99f6b9146af41534c064771e8c49852a9
zjj <544562474@qq.com>
2024-11-05 21:01:10 +0800

    qgroupbox

---
+++ 加载qgroupbox.py
@@ -0,0 +1,12 @@
+import sys
+
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    ui = uic.loadUi("./ui/qgroupbox.ui")
+    ui.show()
+
+    app.exec()
